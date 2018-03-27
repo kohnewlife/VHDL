280836176b994d12ffb730e1833f4a547df91d93
voxqhuy <kohnewlife@gmail.com>
2018-03-26 23:26:15 -0400

    implemented mips alu

--- mips_alu.vhd
+++ mips_alu.vhd
@@ -9,3 +9,40 @@ ENTITY mips_alu IS
 			Zero					: OUT	STD_LOGIC;
 			ALU_Result  		: OUT	STD_LOGIC_VECTOR(31 DOWNTO 0) );
 END mips_alu;
+
+ARCHITECTURE arch OF mips_alu IS 
+	SIGNAL result: STD_LOGIC_VECTOR(31 DOWNTO 0);
+	
+BEGIN
+	PROCESS(ALUControl, inputA, inputB, shamt)
+	BEGIN
+		CASE ALUControl IS
+			WHEN X"0" =>		-- add
+				result <= inputA + inputB;
+			WHEN X"1" =>		-- sub
+				result <= inputA - inputB;
+			WHEN X"2" =>		-- and
+				result <= inputA AND inputB;
+			WHEN X"3" =>		-- or
+				result <= inputA OR inputB;
+			WHEN X"4" =>		
+				IF (inputA < inputB) THEN
+					result <= X"00000001";
+				ELSE 
+					result <= X"00000000";
+				END IF;
+			WHEN OTHERS =>
+				result <= inputA + inputB;		-- add
+		END CASE;
+	END PROCESS;
+	-- Zero
+	IF (result = X"00000000") THEN
+		Zero <= '1';
+	ELSE
+		Zero <= '0';
+	END IF;
+	ALU_Result <= result;
+END arch;
+				
+		
+	
