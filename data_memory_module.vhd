5c467da3bb06b26c9064e1cfa222d0758633e8b2
voxqhuy <kohnewlife@gmail.com>
2018-03-30 09:52:55 -0400

    fixed port

--- data_memory_module.vhd
+++ data_memory_module.vhd
@@ -3,11 +3,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ENTITY mips_register_file IS
-	PORT(	clock, reset, RegWrite	: IN	STD_LOGIC;
-			read_reg1, read_reg2		: IN	STD_LOGIC_VECTOR( 4 DOWNTO 0);
-			write_reg					: IN	STD_LOGIC_VECTOR( 4 DOWNTO 0);
-			write_data					: IN	STD_LOGIC_VECTOR(31 DOWNTO 0);
-			read_data1, read_data2  : OUT	STD_LOGIC_VECTOR(31 DOWNTO 0) );
+	PORT(	clock, reset				: IN	STD_LOGIC;
+			read_address				: IN	STD_LOGIC_VECTOR( 4 DOWNTO 0);
+			write_address				: IN	STD_LOGIC_VECTOR( 4 DOWNTO 0);
+			data_in						: IN	STD_LOGIC_VECTOR(31 DOWNTO 0);
+			data_out					: OUT	STD_LOGIC_VECTOR(31 DOWNTO 0) );
 END mips_register_file;
 
 ARCHITECTURE arch OF mips_register_file IS
