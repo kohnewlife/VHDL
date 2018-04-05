70f184fda7be228099c21a439e5a86859c263d2e
voxqhuy <kohnewlife@gmail.com>
2018-04-05 17:42:03 -0400

    added all components

--- mips.vhd
+++ mips.vhd
@@ -11,14 +11,14 @@ END mips;
 ARCHITECTURE arc OF signExtImmediate IS
 	SIGNAL leading	: STD_LOGIC_VECTOR ( 15 DOWNTO 0 );
 	
-	COMPONENT mipsControl			PORT (	opcode					:	IN	STD_LOGIC_VECTOR(5 DOWNTO 0);
-														funct						:	IN STD_LOGIC_VECTOR(5 DOWNTO 0);
-														RegDst, ALUSrc			:	OUT STD_LOGIC;
-														Jump, jal, Jr			:	OUT STD_LOGIC;
-														Beq, Bne					:	OUT STD_LOGIC;
-														MemRead, MemWrite		:	OUT STD_LOGIC;
-														RegWrite, MemtoReg	:	OUT STD_LOGIC;
-														ALUControl				:  OUT STD_LOGIC_VECTOR(3 DOWNTO 0)	);
+	COMPONENT mipsControl			PORT (	opcode						:	IN	STD_LOGIC_VECTOR(5 DOWNTO 0);
+														funct							:	IN STD_LOGIC_VECTOR(5 DOWNTO 0);
+														RegDst, ALUSrc				:	OUT STD_LOGIC;
+														Jump, jal, Jr				:	OUT STD_LOGIC;
+														Beq, Bne						:	OUT STD_LOGIC;
+														MemRead, MemWrite			:	OUT STD_LOGIC;
+														RegWrite, MemtoReg		:	OUT STD_LOGIC;
+														ALUControl					:  OUT STD_LOGIC_VECTOR(3 DOWNTO 0)	);
 														END COMPONENT;
 													
 	COMPONENT mips_register_file	PORT (	clock, reset, RegWrite	: IN	STD_LOGIC;
@@ -27,7 +27,34 @@ ARCHITECTURE arc OF signExtImmediate IS
 														write_data					: IN	STD_LOGIC_VECTOR(31 DOWNTO 0);
 														read_data1, read_data2  : OUT	STD_LOGIC_VECTOR(31 DOWNTO 0) );
 														END COMPONENT;
- 
+														
+	COMPONENT signExtImmediate 	PORT ( 	input							: IN STD_LOGIC_VECTOR ( 15 DOWNTO 0 );
+														output 						: OUT STD_LOGIC_VECTOR ( 31 DOWNTO 0 ) );
+														END COMPONENT;
+														
+	COMPONENT mips_alu 				PORT (	ALUControl					: IN	STD_LOGIC_VECTOR( 3 DOWNTO 0);
+														inputA, inputB				: IN	STD_LOGIC_VECTOR(31 DOWNTO 0);
+														shamt							: IN	STD_LOGIC_VECTOR( 4 DOWNTO 0);
+														Zero							: OUT	STD_LOGIC;
+														ALU_Result  				: OUT	STD_LOGIC_VECTOR(31 DOWNTO 0) );
+														END COMPONENT;
+
+	COMPONENT ram 						PORT (	address						: IN STD_LOGIC_VECTOR (11 DOWNTO 0);
+														clock							: IN STD_LOGIC  := '1';
+														data							: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+														wren							: IN STD_LOGIC ;
+														q								: OUT STD_LOGIC_VECTOR (31 DOWNTO 0) );
+														END COMPONENT;												
+
+	COMPONENT rom						PORT (	address						: IN STD_LOGIC_VECTOR (11 DOWNTO 0);
+														clock							: IN STD_LOGIC  := '1';
+														q								: OUT STD_LOGIC_VECTOR (31 DOWNTO 0) );
+														END COMPONENT;		
+														
+	U1 : mux21 PORT MAP(a0 => a_input,
+		 a1 => b_input,
+		 s0 => sel(0),
+		 y => temp0);
 BEGIN
 	PROCESS ( input, leading )
 		BEGIN
