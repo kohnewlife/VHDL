57945e96929d9795b76d9d074cc82fe6c13b7cce
voxqhuy <kohnewlife@gmail.com>
2018-04-05 17:11:25 -0400

    added components

--- mips.vhd
+++ mips.vhd
@@ -10,8 +10,23 @@ END mips;
 
 ARCHITECTURE arc OF signExtImmediate IS
 	SIGNAL leading	: STD_LOGIC_VECTOR ( 15 DOWNTO 0 );
-	COMPONENT mux21 PORT( a0,a1,s0 : IN STD_LOGIC;
- y : OUT STD_LOGIC); END COMPONENT;
+	
+	COMPONENT mipsControl			PORT (	opcode					:	IN	STD_LOGIC_VECTOR(5 DOWNTO 0);
+														funct						:	IN STD_LOGIC_VECTOR(5 DOWNTO 0);
+														RegDst, ALUSrc			:	OUT STD_LOGIC;
+														Jump, jal, Jr			:	OUT STD_LOGIC;
+														Beq, Bne					:	OUT STD_LOGIC;
+														MemRead, MemWrite		:	OUT STD_LOGIC;
+														RegWrite, MemtoReg	:	OUT STD_LOGIC;
+														ALUControl				:  OUT STD_LOGIC_VECTOR(3 DOWNTO 0)	);
+														END COMPONENT;
+													
+	COMPONENT mips_register_file	PORT (	clock, reset, RegWrite	: IN	STD_LOGIC;
+														read_reg1, read_reg2		: IN	STD_LOGIC_VECTOR( 4 DOWNTO 0);
+														write_reg					: IN	STD_LOGIC_VECTOR( 4 DOWNTO 0);
+														write_data					: IN	STD_LOGIC_VECTOR(31 DOWNTO 0);
+														read_data1, read_data2  : OUT	STD_LOGIC_VECTOR(31 DOWNTO 0) );
+														END COMPONENT;
  
 BEGIN
 	PROCESS ( input, leading )
