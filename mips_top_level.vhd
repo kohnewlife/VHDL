fb1fea1c9ff425f927efbb994e8f2b54006b2efb
voxqhuy <kohnewlife@gmail.com>
2018-04-05 23:18:43 -0400

    delete redundant files

--- mips_top_level.vhd
+++
@@ -1,9 +0,0 @@
-ENTITY mips IS
-	PORT(	reset							:	IN	STD_LOGIC;
-			slow_clock, fast_clock			:	IN	STD_LOGIC;
-			PC_out, Instruction_out			:	OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-			Read_reg1_out, Read_reg2_out	:	OUT STD_LOGIC_VECTOR(4  DOWNTO 0);
-			Write_reg_out					:	OUT STD_LOGIC_VECTOR(4  DOWNTO 0);
-			Read_data1_out, Read_data2_out	:	OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-			Write_data_out					:	OUT STD_LOGIC_VECTOR(31 DOWNTO 0) );
-END mips;
